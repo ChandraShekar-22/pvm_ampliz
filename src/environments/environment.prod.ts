2629a2da065fd5fffc8d6bdad6dcbf39823b9181
Pranali <pranali@CIPL-MacBook-Pranali.local>
2023-06-13 17:05:39 +0530

    multiple api call issue fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
-  prodAdbApi: "https://go.ampliz.com",
-  prodHcApi: "https://hcapi.ampliz.com",
-  prodB2BApi: "http://119.8.182.178:9000",
+  prodAdbApi: 'https://go.ampliz.com',
+  prodHcApi: 'https://hcapi.ampliz.com',
+  prodB2BApi: 'http://119.8.182.178:9000',
+  prodNPIApi: 'https://hcapi.ampliz.com',
+  prodGlobalB2BApi: 'https://test.ampliz.com',
 };
