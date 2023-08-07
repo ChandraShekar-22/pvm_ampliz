02484382dcdc1b07c82f13f80edfab00414e2202
Pranali <pranali@Ampliz-Pranali.local>
2023-08-07 11:02:10 +0530

    label change

--- src/app/modules/healthcare/containers/physician-overview/physician-overview.component.ts
+++ src/app/modules/healthcare/containers/physician-overview/physician-overview.component.ts
@@ -1,34 +1,34 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core'
-import * as moment from 'moment'
-import { AmplizService } from 'src/app/modules/healthcare/services/ampliz.service'
-import { ActivatedRoute, Router } from '@angular/router'
-import { param } from 'jquery'
-import { MessageService } from '../../../B2B/services/message.service'
-import { DataService } from '../../services/data.service'
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as moment from 'moment';
+import { AmplizService } from 'src/app/modules/healthcare/services/ampliz.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { param } from 'jquery';
+import { MessageService } from '../../../B2B/services/message.service';
+import { DataService } from '../../services/data.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
 	selector: 'app-physician-overview',
 	templateUrl: './physician-overview.component.html',
-	styleUrls: ['./physician-overview.component.css'],
+	styleUrls: ['./physician-overview.component.css']
 })
 export class PhysicianOverviewComponent implements OnInit, AfterViewInit {
-	columnDefs: any
-	tab = 1
-	overviewResult: any
-	physicianOverviewResult: any
-	paramsData: any
-	DataResult: any
-	subscriptions = []
-	headerData: any = ''
-	subscribed: boolean
-	currentCredit: any
-	saveDrawer: boolean = false
-	notCorrectDrawer: boolean = false
-	showButtonLoader: boolean = false
-	physicianPractiseHospitals: any[] = []
-	affiliatedHospitals: any[] = []
-	similarPhysician: any[] = []
-	selectedTab = 1
+	columnDefs: any;
+	tab = 1;
+	overviewResult: any;
+	physicianOverviewResult: any;
+	paramsData: any;
+	DataResult: any;
+	subscriptions = [];
+	headerData: any = '';
+	subscribed: boolean;
+	currentCredit: any;
+	saveDrawer: boolean = false;
+	notCorrectDrawer: boolean = false;
+	showButtonLoader: boolean = false;
+	physicianPractiseHospitals: any[] = [];
+	affiliatedHospitals: any[] = [];
+	similarPhysician: any[] = [];
+	selectedTab = 1;
 	constructor(
 		private activatedRoute: ActivatedRoute,
 		public amplizService: AmplizService,
@@ -38,70 +38,70 @@ export class PhysicianOverviewComponent implements OnInit, AfterViewInit {
 	) {
 		this.columnDefs = [
 			{
-				headerName: 'Drug Name',
+				headerName: 'Drug Name'
 			},
 			{
-				headerName: 'Total claim',
+				headerName: 'Total claim'
 			},
 			{
-				headerName: 'Total Drug Cost',
+				headerName: 'Total Drug Cost'
 			},
 			{
-				headerName: 'Total Drug Cost 65+',
-			},
-		]
+				headerName: 'Total Drug Cost 65+'
+			}
+		];
 	}
 
 	get phyHosInfo() {
-		return this.physicianOverviewResult.physicianHospitalInfoData
+		return this.physicianOverviewResult.physicianHospitalInfoData;
 	}
 	getMapUrl(hospitalLocation: string) {
 		return this.domSanitizer.bypassSecurityTrustResourceUrl(
 			`https://maps.google.com/maps?&q=${hospitalLocation.replace(/,/g, '')}&output‌=embed`
-		)
+		);
 	}
 
 	get phyDrugInfo() {
-		return this.DataResult
+		return this.DataResult;
 	}
 	get languages() {
-		return this.physicianOverviewResult?.physicianInfoData?.languages?.join(', ') || ''
+		return this.physicianOverviewResult?.physicianInfoData?.languages?.join(', ') || '';
 	}
 	get showSimilarPhysicianTab() {
-		return this.similarPhysician.length !== 0
+		return this.similarPhysician.length !== 0;
 	}
 	viewPhysicianMobileNumber() {
 		this.amplizService
 			.viewPhysicianMobileNumber(this.physicianOverviewResult.physicianInfoData.physicianId)
 			.subscribe((res) => {
-				this.viewDetail()
-			})
+				this.viewDetail();
+			});
 	}
 	viewPhysicianEmail() {
 		this.amplizService
 			.viewPhysicianEmail(this.physicianOverviewResult.physicianInfoData.physicianId)
 			.subscribe((res) => {
-				this.viewDetail()
-			})
+				this.viewDetail();
+			});
 	}
 	getPhysicianPractiseHospital() {
 		this.amplizService.getPhysicianPractiseHospital(this.paramsData).subscribe((res) => {
-			this.physicianPractiseHospitals = res.physicianPractiseHospitals
-		})
+			this.physicianPractiseHospitals = res.physicianPractiseHospitals;
+		});
 	}
 	getPhysicianAffiliatedHospital() {
 		this.amplizService.getPhysicianAffiliatedHospital(this.paramsData).subscribe((res) => {
-			this.affiliatedHospitals = res.affiliatedHospitals
-		})
+			this.affiliatedHospitals = res.affiliatedHospitals;
+		});
 	}
 	getSmiliarPhysician(speciality: string) {
 		this.amplizService.getSmiliarPhysician({ physicianId: this.paramsData, speciality }).subscribe((res) => {
-			this.similarPhysician = res.similarPhysicianList
-		})
+			this.similarPhysician = res.similarPhysicianList;
+		});
 	}
 
 	get gender() {
-		return this.physicianOverviewResult.physicianInfoData.gender === 'M' ? 'Male' : 'Female'
+		return this.physicianOverviewResult.physicianInfoData.gender === 'M' ? 'Male' : 'Female';
 	}
 	get isBlankHospitalInfo() {
 		if (
@@ -112,9 +112,9 @@ export class PhysicianOverviewComponent implements OnInit, AfterViewInit {
 			!this.phyHosInfo.city &&
 			!this.phyHosInfo.address
 		) {
-			return true
+			return true;
 		} else {
-			return false
+			return false;
 		}
 	}
 
@@ -127,10 +127,26 @@ export class PhysicianOverviewComponent implements OnInit, AfterViewInit {
 	// }
 
 	get showSaveButton() {
-		return this.physicianOverviewResult.physicianInfoData.leadSaveStatus !== 'Saved'
+		return this.physicianOverviewResult.physicianInfoData.leadSaveStatus !== 'Saved';
+	}
+	get hasEmail() {
+		return (
+			this.physicianOverviewResult?.physicianInfoData?.emailAddress &&
+			this.physicianOverviewResult?.physicianInfoData?.emailAddress?.length !== 0
+		);
+	}
+	get hasPhoneNumber() {
+		return (
+			this.physicianOverviewResult?.physicianInfoData?.phoneNumber &&
+			this.physicianOverviewResult?.physicianInfoData?.phoneNumber?.length !== 0
+		);
 	}
 	get saveButtonText() {
-		return this.physicianOverviewResult.physicianInfoData.leadSaveStatus == 'NotSaved' ? 'View' : 'Save'
+		return this.hasEmail || this.hasPhoneNumber
+			? 'Request'
+			: this.physicianOverviewResult.physicianInfoData.leadSaveStatus == 'NotSaved'
+			? 'View'
+			: 'Save';
 	}
 	get islocationAvailable() {
 		return (
@@ -138,133 +154,135 @@ export class PhysicianOverviewComponent implements OnInit, AfterViewInit {
 			this.physicianOverviewResult.physicianInfoData.address2 ||
 			this.physicianOverviewResult.physicianInfoData.city ||
 			this.physicianOverviewResult.physicianInfoData.country
-		)
+		);
 	}
 	ngOnInit() {
-		this.paramsData = this.activatedRoute.snapshot.params['physicianId']
-		this.getPhysicianOverviewData()
-		this.getPrescriberDrug()
-		this.getPhysicianPractiseHospital()
-		this.getPhysicianAffiliatedHospital()
+		this.paramsData = this.activatedRoute.snapshot.params['physicianId'];
+		this.getPhysicianOverviewData();
+		this.getPrescriberDrug();
+		this.getPhysicianPractiseHospital();
+		this.getPhysicianAffiliatedHospital();
 	}
 	ngAfterViewInit() {
-		this.getDashboardDetails()
+		this.getDashboardDetails();
 	}
 	ngTab(value: any) {
-		this.tab = value
+		this.tab = value;
 	}
 	cancelBtnClick(value: boolean) {
-		this.saveDrawer = value
-		this.notCorrectDrawer = value
+		this.saveDrawer = value;
+		this.notCorrectDrawer = value;
 	}
 	getPhysicianOverviewData() {
 		this.amplizService.physicianOverviewDetails(this.paramsData).subscribe((res) => {
-			this.overviewResult = res
+			this.overviewResult = res;
 
-			this.physicianOverviewResult = res.physicianOverviewInfo
-			this.getSmiliarPhysician(this.physicianOverviewResult.physicianInfoData.specialty)
-		})
+			this.physicianOverviewResult = res.physicianOverviewInfo;
+			this.getSmiliarPhysician(this.physicianOverviewResult.physicianInfoData.specialty);
+		});
 	}
 	refreshedData(ev: any) {
 		if (ev === true) {
-			this.getPhysicianOverviewData()
+			this.getPhysicianOverviewData();
 		}
 	}
 	tabselected(tab: number) {
-		this.selectedTab = tab
+		this.selectedTab = tab;
 	}
 
 	tabClick(ev: any) {
-		var tabLabel = ev.tab.textLabel
+		var tabLabel = ev.tab.textLabel;
 		if (tabLabel === 'Hospital Information') {
-			this.getPhysicianOverviewData()
+			this.getPhysicianOverviewData();
 		} else if (tabLabel === "This Prescriber's Drugs") {
-			this.getPrescriberDrug()
+			this.getPrescriberDrug();
 		}
 	}
 	async getDashboardDetails() {
-		const authToken = await localStorage.getItem('auth_token')
+		const authToken = await localStorage.getItem('auth_token');
 		// const userId = await localStorage.getItem('user_id');
-		const refreshToken = await localStorage.getItem('refresh_token')
+		const refreshToken = await localStorage.getItem('refresh_token');
 		//
 		if (authToken !== null && refreshToken !== null) {
 			this.amplizService.getDashboardDetails().subscribe(
 				(res) => {
-					this.subscriptions = res.Subscriptions
-					this.currentCredit = res.CurrentCredits
+					this.subscriptions = res.Subscriptions;
+					this.currentCredit = res.CurrentCredits;
 					if (this.subscriptions[0].SubscriptionType == 'Free') {
-						localStorage.setItem('SubscriptionisActive', 'false')
-						this.subscribed = false
+						localStorage.setItem('SubscriptionisActive', 'false');
+						this.subscribed = false;
 
-						this.headerData = 'Request Pricing'
+						this.headerData = 'Request Pricing';
 					}
 					if (this.subscriptions[0].SubscriptionType == 'Paid') {
-						localStorage.setItem('SubscriptionisActive', 'true')
-						this.subscribed = true
+						localStorage.setItem('SubscriptionisActive', 'true');
+						this.subscribed = true;
 					}
 				},
 				(error) => {
 					if (error.status === 401) {
-						this.amplizService.logout()
+						this.amplizService.logout();
 					}
 					//
 				}
-			)
+			);
 		} else {
-			this.amplizService.logout()
+			this.amplizService.logout();
 		}
 	}
 
 	getPrescriberDrug() {
 		this.amplizService.getPhysicianPrescriberDrugDetail(this.paramsData).subscribe((el: any) => {
-			this.DataResult = el.physicianDrugInfo
-		})
+			this.DataResult = el.physicianDrugInfo;
+		});
 	}
 
 	request(request: any) {
 		const body = {
 			comid: '0',
 			url: window.location.href,
-			intentrequest: request,
-		}
+			intentrequest: request
+		};
 		this.amplizService.request_access(body).subscribe((res) => {
-			this.messageService.display(true, res.msg)
-		})
+			this.messageService.display(true, res.msg);
+		});
 	}
 
 	viewPhysicianFromList() {
-		const physicianId = this.physicianOverviewResult.physicianInfoData.physicianId
-		this.showButtonLoader = true
+		const physicianId = this.physicianOverviewResult.physicianInfoData.physicianId;
+		this.showButtonLoader = true;
 		this.amplizService.viewPhysicianFromList(physicianId, null).subscribe(
 			(res) => {
-				this.viewDetail()
+				this.viewDetail();
 			},
 			(err) => {
-				this.showButtonLoader = false
+				this.showButtonLoader = false;
 			}
-		)
+		);
 	}
 	viewDetail() {
 		this.amplizService
 			.physicianOverviewDetails(this.physicianOverviewResult.physicianInfoData.physicianId)
 			.subscribe(
 				(res) => {
-					this.showButtonLoader = false
-					this.overviewResult = res
-					this.physicianOverviewResult = res.physicianOverviewInfo
+					this.showButtonLoader = false;
+					this.overviewResult = res;
+					this.physicianOverviewResult = res.physicianOverviewInfo;
 				},
 				(err) => {
-					this.showButtonLoader = false
+					this.showButtonLoader = false;
 				}
-			)
+			);
 	}
 
 	handleSaveButtonPress() {
-		const leadSaveStatus = this.physicianOverviewResult.physicianInfoData.leadSaveStatus
-		if (leadSaveStatus == 'NotSaved') {
-			this.viewPhysicianFromList()
+		const leadSaveStatus = this.physicianOverviewResult.physicianInfoData.leadSaveStatus;
+		if (this.saveButtonText === 'Request') {
+			this.request('Request for Phone And Email');
+		} else if (leadSaveStatus == 'NotSaved') {
+			this.viewPhysicianFromList();
 		} else {
-			this.saveDrawer = true
+			this.saveDrawer = true;
 		}
 	}
 }
